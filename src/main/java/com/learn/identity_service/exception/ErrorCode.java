a333396b1a4252302561a5cd3b0f28b857897090
vynnd <nguynhudangvy5.10.1998@gmail.com>
2024-11-08 09:36:20 +0700

    Exception Handler and return response with HttpCode

--- src/main/java/com/learn/identity_service/exception/ErrorCode.java
+++ src/main/java/com/learn/identity_service/exception/ErrorCode.java
@@ -1,30 +1,30 @@
 package com.learn.identity_service.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
 
-    INVALID_KEY(1001,"Invalid message key"),
-    USER_EXISTED(1002, "User existed"),
-    USERNAME_INVALID(1003,"User name must be least 3 characters"),
-    PASSWORD_INVALID(1004,"Password must be least 8 characters"),
-    UNCATEGORIZED_EXISTED(9999, "Uncategorized exception"),
-    USER_NOT_EXISTED(1005, "User not existed"),
-    UNAUTHENTICATED(1006, "Unauthenticated"),
+    INVALID_KEY(1001,"Invalid message key", HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003,"User name must be least 3 characters", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004,"Password must be least 8 characters", HttpStatus.BAD_REQUEST),
+    UNCATEGORIZED_EXISTED(9999, "Uncategorized exception", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_EXISTED(1005, "User not existed", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
     ;
 
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
     private int code;
     private String message;
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    private HttpStatusCode statusCode;
 }
--- src/main/java/com/learn/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/learn/identity_service/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.learn.identity_service.exception;
 
 import com.learn.identity_service.dto.request.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,7 +24,19 @@ public class GlobalExceptionHandler {
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity
+                .status(errorCode.getStatusCode())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception){
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
@@ -40,6 +53,6 @@ public class GlobalExceptionHandler {
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
     }
 }
